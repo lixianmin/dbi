f15b5c4913725025186a591e0cac10fd2112d569
lixianmin <lixianmin@live.cn>
2020-06-23 15:19:24 +0800

    加入接口类

--- ifs.go
+++ ifs.go
@@ -1,8 +1,18 @@
 package dbi
 
+import "database/sql"
+
 /********************************************************************
 created:    2020-06-18
 author:     lixianmin
 
 Copyright (C) - All Rights Reserved
  *********************************************************************/
+
+type IQueryContext interface {
+	QueryContext(ctx *Context, query string, args ...interface{}) (*sql.Rows, error)
+}
+
+type IExecContext interface {
+	ExecContext(ctx *Context, query string, args ...interface{}) (sql.Result, error)
+}
